7c8de87df37bab049e52a9a0a45ef5c5070c183b
lovelyshu <159614549+lovely-shu@users.noreply.github.com>
2025-04-09 16:08:42 +0900

    Delete src directory

--- src/common/Empty.tsx
+++
@@ -1,7 +0,0 @@
-import { Box } from "@mui/material";
-
-const Empty = () => {
-  return <Box width={"100%"} height={"100px"}></Box>;
-};
-
-export default Empty;
--- src/components/MainImg.tsx
+++
@@ -1,6 +0,0 @@
-import Box from "@mui/material/Box";
-const MainImg = () => {
-  return <Box></Box>;
-};
-
-export default MainImg;
